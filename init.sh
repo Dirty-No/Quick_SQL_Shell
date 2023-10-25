87ba2c5319daa4a05433d7eb20ca81fa02131114
anon <do-not-leak@e>
2023-10-25 11:01:39 +0200

    docs(oracle): Update comments

--- init.sh
+++ init.sh
@@ -8,7 +8,8 @@ butterfly.server.py --uri-root-path=postgres --cmd="bash -il -c psql"  --host=0.
 butterfly.server.py --uri-root-path=mysql --cmd="bash -il -c mysql"  --host=0.0.0.0 --port=8882 --i-hereby-declare-i-dont-want-any-security-whatsoever &
 sudo -u sqlite butterfly.server.py --uri-root-path=sqlite --cmd="bash -il -c sqlite3"  --host=0.0.0.0 --port=8883 --i-hereby-declare-i-dont-want-any-security-whatsoever &
 
-# Warning: Butterfly seems to handle nested quotes badly, so we use a very dirty trick to avoid them simply
+# Warning: Butterfly seems to handle nested quotes badly, so we use this very dirty trick to avoid them simply.
 # The oracle database may take longer to start.
+# rlwrap allows to have interactive features like history, line editing for non-interactive CLI applications.
 echo >>~/.bashrc 'alias oracle="rlwrap sqlplus $ORACLE_USER/$ORACLE_PASSWORD@$ORACLE_HOST:$ORACLE_PORT/$ORACLE_PDB"'
 butterfly.server.py --uri-root-path=oracle --cmd="bash -il -c oracle" --host=0.0.0.0 --port=8884 --i-hereby-declare-i-dont-want-any-security-whatsoever
