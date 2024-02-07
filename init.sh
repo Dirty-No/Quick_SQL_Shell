7157e52f5877b3508dc35b5ca8767fe553eee058
anon <do-not-leak@e>
2024-02-07 11:28:52 +0100

    refactor(webshells): Refactor init script and set sqlite to safe mode.

--- init.sh
+++ init.sh
@@ -3,15 +3,37 @@
 # Export environment variables to a file for persistence
 export > ~/.bashrc
 
-# Start Butterfly server instances for each database
-butterfly.server.py --uri-root-path=postgres --cmd="bash -il -c psql"  --host=0.0.0.0 --port=8881 --i-hereby-declare-i-dont-want-any-security-whatsoever &
-butterfly.server.py --uri-root-path=mysql --cmd="bash -il -c mysql"  --host=0.0.0.0 --port=8882 --i-hereby-declare-i-dont-want-any-security-whatsoever &
-sudo -u sqlite butterfly.server.py --uri-root-path=sqlite --cmd="bash -il -c sqlite3"  --host=0.0.0.0 --port=8883 --i-hereby-declare-i-dont-want-any-security-whatsoever &
+start_webshell() {
+    ROOT_PATH="$1"
+    PORT="$2"
+    COMMAND="$3"
 
-# Warning: Butterfly seems to handle nested quotes badly, so we use this very dirty trick to avoid them simply.
-# The oracle database may take longer to start.
-# rlwrap allows to have interactive features like history, line editing for non-interactive CLI applications.
-echo 'rlwrap sqlplus $ORACLE_USER/$ORACLE_PASSWORD@$ORACLE_HOST:$ORACLE_PORT/$ORACLE_PDB' > /tmp/oracle-script.sh
+    # Butterfly seems to be buggy when passing quotes and stuff to --cmd, so the best way to bypass this issue
+    # is to write the command in a temporary shell script in execute is with bash.
+    CMD_FILE=$(mktemp)
+    echo "$3" > "$CMD_FILE"
 
-# WARN: Oracle takes a lot of time to boot, make sure you have waited long enough before thinking you need to debug something
-butterfly.server.py --uri-root-path=oracle --cmd="bash -il /tmp/oracle-script.sh" --host=0.0.0.0 --port=8884 --i-hereby-declare-i-dont-want-any-security-whatsoever
+    # The security flag is required to allow exposing butterfly without password and SSL (which you should handle with a reverse-proxy)
+    butterfly.server.py \
+        --cmd="bash -il $CMD_FILE" \
+        --uri-root-path="$ROOT_PATH" \
+        --host=0.0.0.0 \
+        --port="$PORT" \
+        --i-hereby-declare-i-dont-want-any-security-whatsoever &
+}
+
+# Start postgres shell (args in env)
+start_webshell postgres 8881 psql
+
+# Start mysql shell (args in env)
+start_webshell mysql 8882 mysql
+
+# Start sqlite shell (-safe forbids escapes like using .shell/.system commands), no other args needed
+start_webshell sqlite 8883 'sqlite3 -safe'
+
+# Start oracle shell, sqlplus doesn't seem to use readline for interactive line editing, history etc, 
+# so we add it ourselves using rlwrap
+start_webshell oracle 8884 'rlwrap sqlplus $ORACLE_USER/$ORACLE_PASSWORD@$ORACLE_HOST:$ORACLE_PORT/$ORACLE_PDB'
+
+# Webshells run in background, only exit if all of them have died.
+wait
